e10fc50fa01ec572c8f9b2f3f6e69151c6034858
Szymon Brodawka <szymbrod@gmail.com>
2023-11-07 15:10:50 +0100

    Adding function convertin numbers to integers or floats and adding function saving inputs to arrys

--- scripts/main.js
+++ scripts/main.js
@@ -1,52 +1,100 @@
 const calculatorInputs = [];
-
-const calculate = () => {
-    if(calculatorInputs.length < 3)
-    {
-        return null;
-    }
-
-    let result = 0;
-    for(let i = 0; i < calculatorInputs.length - 2; i += 3) {
-        if(checkIfIsNumber(calculatorInputs[i]) && checkIfIsNumber(calculatorInputs[i+2]))
-        {
-            result += parseInt(calculatorInputs[i] + calculatorInputs[i+2]);
-        }
-    }
-
-    return result;
-
-}
-
-const showResultOnScreen = (event) => {
-    if(!event.target.classList.contains("result-button"))
-    {
-        return;
-    }
-
-    const resultScreen = document.querySelector(".result-screen");
-    //if calculates returns null then show error and return nothing
-    
-    resultScreen.textContent = calculate(calculatorInputs);
-};
-
-const saveCalculatorInput = (event) => {
-    if(!event.target.classList.contains("button"))
-    {
-        return;
-    }
-
-    calculatorInputs.push(event.target.textContent);
-}
+const numberInputs = [];
+const operationInputs = [];
+let numberInput = "0";
+let result;
 
 const checkIfIsNumber = (input) => {
     const numberPattern = /\d+/;
-    const isANumber = numberPattern.test(input);
+    return numberPattern.test(input);
+}
 
-    return isANumber;
+const checkIfIsOperationSign = (input) => {
+    const operationSignPattern = /(\+|\-|x|\/)/;
+    return operationSignPattern.test(input);
+}
+
+const convertNumberType = (number) => {
+    if(number.length < 2)
+        return 0;
+    
+    number = number.slice(1, number.length);
+    number.includes(".")
+        ? number = parseFloat(number)
+        : number = parseInt(number);
+    return number;
+}
+
+const showResultOnScreen = (result) => {
+    const resultScreen = document.querySelector(".result-screen");
+    resultScreen.textContent = result;
+};
+
+const calculate = (numberInputs, operationInputs) => {
+}
+
+const saveInput = (event) => {
+    const clickedObject = event.target;
+    if(!clickedObject.classList.contains("button"))
+    {
+        return;
+    }
+
+    const input = clickedObject.textContent;
+    if(checkIfIsNumber(input)
+        || input === "." && !numberInput.includes(".")
+        || (input === "-" && !numberInput.includes("-") && numberInput.length < 2)
+    )
+    {
+        numberInput += input;
+    }
+
+    if(checkIfIsOperationSign(input) && numberInput.length > 2)
+    {
+        numberInputs.push(convertNumberType(numberInput));
+        operationInputs.push(input);
+        numberInput = "0";
+    }
+
+    const previousInput = operationInputs[operationInputs.length - 1];
+    if(checkIfIsOperationSign(previousInput) && input === "=")
+    {
+        numberInputs.push(convertNumberType(numberInput));
+        numberInput = "0";
+    }
+
+    if(checkIfIsNumber(previousInput) && input === "=")
+    {
+        calculate(numberInputs, operationInputs);
+        //clear all arrays
+    }
+
+    console.log(numberInputs);
+    console.log(operationInputs);
+
+    // if(checkIfIsOperationSign(input) && numberInput.length > 2)
+    // {
+    //     calculatorInputs.push(convertNumberType(numberInput));
+    //     calculatorInputs.push(input);
+    //     numberInput = "0";
+    // }
+
+    // const previousInput = calculatorInputs[calculatorInputs.length - 1];
+    // if(checkIfIsOperationSign(previousInput) && input === "=")
+    // {
+    //     calculatorInputs.push(convertNumberType(numberInput));
+    //     numberInput = "0";
+    // }
+
+    // if(checkIfIsNumber(previousInput) && input === "=")
+    // {
+    //     // calculate();
+    // }
+    
+    // console.log(calculatorInputs);
+    // console.log(numberInput);
 }
 
 
 const calculator = document.querySelector(".calculator");
-calculator.addEventListener("click", (event) => showResultOnScreen(event));
-calculator.addEventListener("click", (event) => saveCalculatorInput(event));
+calculator.addEventListener("click", event => saveInput(event));
