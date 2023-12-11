2d969df7353f8b2ab00af78c159142c443892fa2
Szymon Brodawka <szymbrod@gmail.com>
2023-12-11 17:15:26 +0100

    Move function saving calculator input to the saveInput file

---
+++ scripts/saveInput.js
@@ -0,0 +1,53 @@
+import { isOperand } from "./utility.js";
+
+const isKeyboardInputAllowed = (keyboardInput) =>
+{
+    if(keyboardInput === "Backspace")
+    {
+        return true;
+    }
+    
+    if(
+        !isOperand(keyboardInput) && isNaN(keyboardInput) && keyboardInput !== "="
+        && keyboardInput !== "."
+    )
+    {
+        return false;
+    }
+
+    return true;
+}
+
+const saveInputFromKeyboard = (event) =>
+{
+    const inputValue = event.key;
+    if(!isKeyboardInputAllowed(inputValue))
+    {
+        return "";
+    }
+
+    return inputValue;
+}
+
+const saveInputFromCalculator = (event) =>
+{
+    const clickedObject = event.target;
+    const inputValue = clickedObject.value;
+    const wasCalculatorButtonClicked = clickedObject.classList.contains("button");
+    if(!wasCalculatorButtonClicked)
+    {
+        return "";
+    }
+
+    return inputValue;
+}
+
+export const saveInput = (event) =>
+{
+    let inputValue = "";
+    event.type === "click"
+        ? inputValue = saveInputFromCalculator(event)
+        : inputValue = saveInputFromKeyboard(event);
+
+    return inputValue;
+}
